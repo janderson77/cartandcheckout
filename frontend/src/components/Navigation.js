29a0478e54606a0413aa6572e5d190b7087484d1
janderson77 <jamesseventyseven@gmail.com>
2022-04-27 22:19:59 -0600

    Logout works

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,18 +1,53 @@
 import React, {Fragment} from 'react';
+import {useDispatch, useSelector} from 'react-redux'
 import {Navbar, NavItem, NavbarBrand, NavLink} from 'reactstrap'
+import {logout} from '../actions/users'
 
 const Navigation = () => {
+    const dispatch = useDispatch();
+    const user = useSelector((st) => st.users.user)
+
+    const handleLogout = () => {
+        try{
+            dispatch(logout())
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    const loggedInNav = () => {
+        return(
+        <Navbar color='dark'>
+            <NavbarBrand href="/">C&C</NavbarBrand>
+            <NavItem>
+                <NavLink href="/" onClick={handleLogout}>Logout</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/cart">Cart</NavLink>
+            </NavItem>
+        </Navbar>
+        )
+    };
+
+    const loggedOutNav = () => {
+        return(
+        <Navbar color='dark'>
+            <NavbarBrand href="/">C&C</NavbarBrand>
+            <NavItem>
+                <NavLink href="/login">Login</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/cart">Cart</NavLink>
+            </NavItem>
+        </Navbar>
+        )
+    };
+
     return(
-        <div>
-            <Fragment>
-                <Navbar color='dark'>
-                    <NavbarBrand href='/'>C&C</NavbarBrand>
-                    <NavItem>
-                        <NavLink href="/cart">Cart</NavLink>
-                    </NavItem>
-                </Navbar>
-            </Fragment>
-        </div>
+        <Fragment>
+                {user ? loggedInNav() : loggedOutNav()}
+        </Fragment>
+        
     )
 };
 
