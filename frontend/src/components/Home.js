bc932127d23eb608d4e152fb498b92f20917cb6c
janderson77 <jamesseventyseven@gmail.com>
2022-04-30 14:37:48 -0600

    Removed excess div from home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,18 +3,16 @@ import {Helmet} from 'react-helmet';
 
 const Home = () => {
     return(
-        <div>
-            <Fragment>
-                <Helmet>
-                    <title>Cart and Checkout Demo</title>
-                </Helmet>
+        <Fragment>
+            <Helmet>
+                <title>Cart and Checkout Demo</title>
+            </Helmet>
 
-                <div className='mt-4 p-5 bg-primary text-white rounded'>
-                    <h1>Cart and Checkout Demo</h1>
-                    <p>Lorem Ipsum...</p>
-                </div>
-            </Fragment>
-        </div>
+            <div className='mt-4 p-5 bg-primary text-white rounded'>
+                <h1>Cart and Checkout Demo</h1>
+                <p>Lorem Ipsum...</p>
+            </div>
+        </Fragment>
     )
 };
 
